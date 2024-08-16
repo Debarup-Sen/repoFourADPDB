50c1319ab985f360cd6c185b1a7e5d1a6b0f17f6
Debarup-Sen <79254755+Debarup-Sen@users.noreply.github.com>
2024-08-17 00:27:59 +0530

    Update ADPDB_ML_Model1.py

--- ADPDB_ML_Model1.py
+++ ADPDB_ML_Model1.py
@@ -54,7 +54,7 @@ sequence = lit.text_area('Please enter the input sequence (Plain text/FASTA/mult
 file_query = lit.file_uploader("Or, you may upload file (Plain text/FASTA/multi-FASTA formats supported)")
 if file_query:
     sequence = StringIO(file_query.getvalue().decode("utf-8")).read().upper()
-    lit.text(f'1 {sequence[:50]}')
+    lit.text(f'1 {sequence[:500]}')
 repeat = 0
 #if lit.checkbox('Do you want list of permutation of all input amino acids?'):
 #    repeat = lit.text_input('What length of peptides should be created? (Please enter integer value)')
